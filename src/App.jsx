17204ad706d7cdd88b6f4d14ff84f6f42815f0e0
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2023-02-22 21:33:38 +0300

    Add reduxjs-toolkit and used it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Catalog } from "./components/Catalog";
-import { Footer } from "./components/Footer";
+import { Provider } from "react-redux";
 import { Header } from "./components/Header";
 import { Navigation } from "./components/Navigation";
+import { Catalog } from "./components/Catalog";
+import { Footer } from "./components/Footer";
+import { store } from "./store";
 
 export const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <main>
         <Navigation />
         <Catalog />
       </main>
       <Footer />
-    </>
+    </Provider>
   );
 };
