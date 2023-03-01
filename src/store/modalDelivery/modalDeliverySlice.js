bb253d42526124326fbe8890ac9db417583fe844
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2023-03-01 13:51:37 +0300

    Added GET request for modal window.

--- src/store/modalDelivery/modalDeliverySlice.js
+++ src/store/modalDelivery/modalDeliverySlice.js
@@ -1,15 +1,8 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { API_URI, POSTFIX } from "../../const";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isOpen: false,
 };
-
-export const modalRequestAsync = createAsyncThunk("modal/fetch", () =>
-  fetch(`${API_URI}${POSTFIX}/category`)
-    .then((req) => req.json())
-    .catch((error) => ({ error }))
-);
 
 const modalSlice = createSlice({
   name: "modal",
